adc869b11c6789ec881de3f91e1221d1265988bf
Mira Trouvain <mira.trouvain@tum.de>
2020-11-16 19:05:54 +0100

    doxygen cmake module (faulty)

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,22 @@
-# make doc_doxygen optional if someone does not have / like doxygen
+ # make doc_doxygen optional if someone does not have / like doxygen
 
-# TODO: create CMake build option for the target.
+ # TODO: create CMake build option for the target.
+ option(DISABLE_DOXYGEN "disables the build of a Doxygen documentation" OFF)
 
-# TODO: Add a custom target for building the documentation.
+ # TODO: Add a custom target for building the documentation.
+
+ #Add target to build documents from visual studio.
+ #set(DOXYGEN_INPUT Doxyfile)
+ #set the output directory of the documentation
+ set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/doxys_documentation)
+ # sanity check...
+ #message("Doxygen Output ${DOXYGEN_OUTPUT_DIR}")
+
+ if(NOT DISABLE_DOXYGEN)
+     ADD_CUSTOM_TARGET(doc_doxygen
+             COMMAND ${CMAKE_COMMAND} -E make_directory ${DOXYGEN_OUTPUT_DIR}
+             ${DOXYGEN_EXECUTABLE} Doxyfile
+             #WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+             VERBATIM
+             )
+ endif(NOT DISABLE_DOXYGEN) custom target for building the documentation.
