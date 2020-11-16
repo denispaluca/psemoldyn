bc87ef79f6aa4aab7f2c655ac43c7b16026e0e91
Mira Trouvain <mira.trouvain@tum.de>
2020-11-16 19:25:50 +0100

    functional version

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -4,19 +4,19 @@
  option(DISABLE_DOXYGEN "disables the build of a Doxygen documentation" OFF)
 
  # TODO: Add a custom target for building the documentation.
-
- #Add target to build documents from visual studio.
- #set(DOXYGEN_INPUT Doxyfile)
+ 
  #set the output directory of the documentation
  set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/doxys_documentation)
- # sanity check...
- #message("Doxygen Output ${DOXYGEN_OUTPUT_DIR}")
+
 
  if(NOT DISABLE_DOXYGEN)
-     ADD_CUSTOM_TARGET(doc_doxygen
+     find_package(Doxygen
+             REQUIRED doxygen)
+
+
+             ADD_CUSTOM_TARGET(doc_doxygen
              COMMAND ${CMAKE_COMMAND} -E make_directory ${DOXYGEN_OUTPUT_DIR}
-             ${DOXYGEN_EXECUTABLE} Doxyfile
-             #WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-             VERBATIM
+             COMMAND Doxygen::doxygen ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
+             COMMENT "Generating documentation using Doxygen" VERBATIM
              )
  endif(NOT DISABLE_DOXYGEN)
