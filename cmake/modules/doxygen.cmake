4b2b5589d82f0dbaba30308f88ae381d18f23d89
denis <ge37dem@mytum.de>
2020-11-17 10:00:49 +0100

    Change doxygen directory to MolSim/doxys_documentation

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -6,14 +6,14 @@ option(DISABLE_DOXYGEN "disables the build of a Doxygen documentation" OFF)
 # TODO: Add a custom target for building the documentation.
 
 if(NOT DISABLE_DOXYGEN)
-     find_package(Doxygen
-             REQUIRED doxygen)
+    find_package(Doxygen
+         REQUIRED doxygen)
 
-     SET(DOXYGEN_OUT ${PROJECT_SOURCE_DIR}/Doxyfile)
+    SET(DOXYGEN_OUT ${PROJECT_SOURCE_DIR}/Doxyfile)
 
     ADD_CUSTOM_TARGET(doc_doxygen ALL
         COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Generating documentation using Doxygen" VERBATIM
     )
 endif(NOT DISABLE_DOXYGEN)
