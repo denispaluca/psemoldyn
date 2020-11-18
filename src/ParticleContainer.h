69cc5fa96eee6fd3ba66db42e816e1d33d702824
denis <ge37dem@mytum.de>
2020-11-18 14:14:33 +0100

    Add comments to ParticleContainer.h

--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -7,47 +7,63 @@
 
 #include "Particle.h"
 #include <vector>
+/**
+ * The ParticleContainer class encapsulates particles
+ * and makes it possible to iterate through them.
+ */
 class ParticleContainer {
 private:
     /**
-     * List of particles
+     * Vector of particles
      */
     std::vector<Particle> particles;
 public:
     /**
-     * Empty init container
+     * Constructs ParticleContainer with empty vector of particles.
+     * @return
+     * @param
      */
     ParticleContainer();
+
     /**
-     * Init container with vector of particles.
+     * Constructs ParticleContainer by copying particles.
+     * @param particles Particles to be copied.
+     * @return
      */
-    ParticleContainer(std::vector<Particle>&);
+    ParticleContainer(std::vector<Particle> &particles);
+
     /**
-     * Init container with filename of file to be parsed.
-     * @param filename
+     * Constructs ParticleContainer with filename to be parsed by FileReader.
+     * @param filename Name of the files which will be parsed.
+     * @return
      */
     ParticleContainer(char* filename);
+
     /**
      * Returns vector of particles in container.
      * @param
      * @return The vector of particles
      */
     std::vector<Particle> &getParticles();
+
     /**
      * Push particle at the end of the vector.
      * @param particle The particle pushed in particles vector
      * @return
      */
     void push(Particle &particle);
+
     /**
      * Iterate over all particles and apply function f
      * @param f Function applied to particles
      * @return
      */
     void iterate(void (*f)(Particle&));
+
     /**
      * Iterate over all unique particle pairs and apply function f
      * @param f Function that is applied to pairs
+     * @return
      */
     void iteratePairs(void (*f)(Particle&,Particle&));
 };
