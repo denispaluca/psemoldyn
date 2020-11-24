5bbab089349d3ef449664030bb54d581e5bd24e9
denis <ge37dem@mytum.de>
2020-11-24 16:58:43 +0100

    Add comments for tests

--- tests/src/ParticleContainerFixture.cpp
+++ tests/src/ParticleContainerFixture.cpp
@@ -4,7 +4,9 @@
 #include <gtest/gtest.h>
 #include "../../src/ParticleContainer.h"
 
-
+/**
+ * Fixture to write tests for the ParticleContainer class.
+ */
 class ParticleContainerFixture : public testing::Test {
 protected:
     ParticleContainer pc;
@@ -18,17 +20,27 @@ protected:
       }
     }
 };
-
+/**
+ * Checks if correct number of particles was pushed.
+ */
 TEST_F(ParticleContainerFixture, Size){
   EXPECT_EQ(pc.getParticles().size(), particles.size());
 }
 
+/**
+ * Checks if the particles pushed are the same as the
+ * as the ones in a simple vector.
+ */
 TEST_F(ParticleContainerFixture, Equality){
   for(int i=0;i<particles.size();i++){
     EXPECT_TRUE(particles.at(i) == pc.getParticles().at(i));
   }
 }
 
+/**
+ * Checks if the iterate function works the same as
+ * a for-loop (also checks if addF works).
+ */
 TEST_F(ParticleContainerFixture, Iterator){
   pc.iterate([](Particle& p){
       std::array<double, 3> f = {1,1,1};
