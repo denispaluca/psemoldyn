deb0408ee51a04a2be1cfce8b99e013358982121
Mira Trouvain <mira.trouvain@tum.de>
2020-11-28 16:51:13 +0100

    added basic help text + option -h/--help + longer options --file/--cuboid

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -34,6 +34,8 @@ void plotParticles(int iteration, std::vector<Particle> &particles);
  */
 double radiusPow3(const std::array<double, 3> &x);
 
+void help();
+
 constexpr double start_time = 0;
 double t_end = 0.0;
 double delta_t = 0.0;
@@ -46,10 +48,17 @@ double delta_t = 0.0;
  */
 int main(int argc, char *argsv[]) {
   std::cout << "Hello from MolSim for PSE!" << std::endl;
+
+  if(argc == 2 && (std::string(argsv[1]) == "-h" || (std::string(argsv[1]) == "--help"))) {
+      help();
+      return 0;
+  }
+
   //TODO: change condition
-  if (argc != 5) {
+  if (argc < 5) {
     std::cout << "Errounous programme call! " << std::endl;
-    std::cout << "./MolSim t_end delta_t {-f filename | -c <number of cuboids> <cuboid data>}" << std::endl;
+    help();
+    //std::cout << "./MolSim t_end delta_t {-f filename | -c <number of cuboids> <cuboid data>}" << std::endl;
   }
 
   double current_time = start_time;
@@ -60,7 +69,7 @@ int main(int argc, char *argsv[]) {
   ParticleGenerator particleGenerator = ParticleGenerator();
 
   std::string input_method = argsv[3];
-  if(input_method == "-f"){
+  if(input_method == "-f" || input_method == "--file"){
       if (std::regex_match(argsv[4], std::regex(".+\\.particles"))) {
           particleContainer = ParticleContainer(argsv[4]);
       } else if (std::regex_match(argsv[4], std::regex(".+\\.cuboids"))) {
@@ -69,14 +78,16 @@ int main(int argc, char *argsv[]) {
       } else {
           //TODO: log error + input help
           std::cout << "Invalid filename or filename extension, must be <name>.particles or <name>.cuboids." << std::endl;
+          help();
           return -1;
       }
-  } else if(input_method == "-c"){
+  } else if(input_method == "-c" || input_method == "--cuboid"){
 
       int num_cuboids = std::stoi(argsv[4]);
       if (argc != num_cuboids * 11 + 5) {           //11 cuboid parameters (w/O mean value of Brownian Motion)
           // TODO: log error + input help
-          std::cout << "Erroneous programme call! " << std::endl;
+          std::cout << "Faulty cuboid data input" << std::endl;
+          help();
           return -1;
       }
 
@@ -104,6 +115,7 @@ int main(int argc, char *argsv[]) {
       particleContainer = particleGenerator.getParticles();
   } else {
       std::cout << "Erroneous programme call!" << std::endl;
+      help();
   }
 
   particleContainer.iterate(
@@ -182,4 +194,24 @@ void plotParticles(int iteration, std::vector<Particle> &particles) {
 double radiusPow3(const std::array<double, 3> &x) {
     double result = x[0]*x[0] + x[1]*x[1] + x[2]*x[2];
     return pow(sqrt(result), 3);
+}
+
+void help() {
+    std::cout << std::endl;
+    std::cout << "This Program should be called as follows:" << std::endl;
+    std::cout << "./MolSim t_end delta_t {-f filename | -c cuboid_number <cuboid_data>...}" << std::endl;
+    std::cout << "---------------------------------------------------------------" << std::endl;
+    std::cout << "t_end:         end time of the simulation" << std::endl;
+    std::cout << "delta_t:       length of time step between iterations" << std::endl;
+    std::cout << "-f, --file:    enables input via file. Accepted file extensions are .particles and .cuboids." << std::endl;
+    std::cout << "               The corresponding file formats are specified in the project's ReadMe." << std::endl;
+    std::cout << "-c, --cuboid:  allows for direct input of cuboid parameters. The data is expected to be given in the following order:" << std::endl;
+    std::cout << "                 <position of cuboid> - 3 double values" << std::endl ;
+    std::cout << "                 <number of particles per dimension> - 3 integer values" << std::endl;
+    std::cout << "                 <mesh width> - 1 double value" << std::endl;
+    std::cout << "                 <mass of 1 particle> - 1 double value" << std::endl;
+    std::cout << "                 <initial velocity> - 3 double values" << std::endl;
+    std::cout << "               divided by blank spaces and in a single line." << std::endl;
+    std::cout << "               Data of multiple cuboids must also be given in a single line with a blank space in between." << std::endl;
+    std::cout << "This help text can be printed using ./MolSim -h or ./MolSim --help." << std::endl;
 }
