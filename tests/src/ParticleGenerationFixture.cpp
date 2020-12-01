515cee263a484b041f1799cf62a76c697bcdc5e5
Mira Trouvain <mira.trouvain@tum.de>
2020-12-01 20:52:38 +0100

    basic tests for functions/classes around particle generation

--- tests/src/ParticleGenerationFixture.cpp
+++ tests/src/ParticleGenerationFixture.cpp
@@ -3,17 +3,20 @@
 //
 #include <gtest/gtest.h>
 #include "../../src/ParticleGenerator.h"
+#include "../../src/FileReader.h"
 
 /**
- * Fixture to write tests for the ParticleContainer class.
+ * Fixture to write tests for //TODO
  */
-class ParticleContainerFixture : public testing::Test {
+class ParticleGenerationFixture : public testing::Test {
 protected:
     ParticleContainer pc;
     std::vector<Particle> particles;
     std::vector<Cuboid> c;
+    std::vector<Cuboid> read;
     ParticleContainer generated;
     ParticleGenerator pg;
+    FileReader reader;
     void SetUp() override {
 
         // 3 Cuboids with 2x2x2 Particles each
@@ -49,5 +52,41 @@ protected:
         for (auto &cuboid : c){
             pg.addCuboid(cuboid);
         }
+
+        std::string test_file_normal = "input/test_reading_normal.cuboids";
+        reader.readCuboids(read, const_cast<char *>(test_file_normal.c_str()));
     }
 };
+
+bool generatedSame(Particle &p1, Particle &p2){
+    return (p1.getX() == p2.getX() && p1.getF() == p2.getF() && p1.getM() == p2.getM() && p1.getType() == p2.getType());
+}
+
+TEST_F(ParticleGenerationFixture, Particle_Generation) {
+    for(int i=0;i<pc.getParticles().size();i++){
+        EXPECT_TRUE(generatedSame(pc.getParticles().at(i), generated.getParticles().at(i)));
+    }
+}
+
+TEST_F(ParticleGenerationFixture, Adding_Cuboids_Same_Cuboids) {
+    for(int i=0;i<c.size();i++){
+        EXPECT_TRUE(c.at(i) == pg.getCuboids().at(i));
+    }
+}
+
+TEST_F(ParticleGenerationFixture, Adding_Cuboids_Same_Particles) {
+    for(int i=0;i<pc.getParticles().size();i++){
+        EXPECT_TRUE(generatedSame(pc.getParticles().at(i), pg.getParticles().getParticles().at(i)));
+    }
+}
+
+TEST_F(ParticleGenerationFixture, Reading_Cuboids_Normal) {
+    for(int i=0;i<c.size();i++){
+        EXPECT_TRUE(c.at(i) == read.at(i));
+    }
+}
+
+TEST_F(ParticleGenerationFixture, Reading_Cuboids_Faulty1) {
+    std::string test_file_normal = "input/test_reading_faulty1.cuboids";
+    ASSERT_DEATH(reader.readCuboids(read, const_cast<char *>(test_file_normal.c_str())), "");
+}
