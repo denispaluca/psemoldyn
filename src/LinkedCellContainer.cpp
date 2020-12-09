cc9db1ca093ba4bfa9cb5d24cfe09c6ef7a9016c
Mira Trouvain <mira.trouvain@tum.de>
2020-12-09 17:15:01 +0100

    Container for cells with constructor, getters + function for deleting halo particles

--- src/LinkedCellContainer.cpp
+++ src/LinkedCellContainer.cpp
@@ -3,23 +3,101 @@
 //
 
 #include "LinkedCellContainer.h"
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
-
-LinkedCellContainer LinkedCellContainer() {
-    cutoff_radius = 1;
-    cells = ;
-
-    std::vector<LinkedCell> boundary;
-
-    std::vector<LinkedCell> halo;
+LinkedCellContainer::LinkedCellContainer() {
+    domain_size = {0.0, 0.0, 0.0};
+    domain_pos = {0.0, 0.0, 0.0};
+    cutoff_radius = 0.0;
+    cells = std::vector<LinkedCell>();
+    inner = std::vector<LinkedCell>();
+    boundary = std::vector<LinkedCell>();
+    halo = std::vector<LinkedCell>();
 }
 
-LinkedCellContainer LinkedCellContainer(double cutoff_radius, ParticleContainer &particles) {
+LinkedCellContainer::LinkedCellContainer(std::array<double, 3> domain_size,
+                                         std::array<double, 3> domain_pos, double cutoff_radius, ParticleContainer &particles) {
+    this->domain_size = domain_size;
+    this->domain_pos = domain_pos;
+    this->cutoff_radius = cutoff_radius;
 
+    int cellsX = std::ceil(domain_size[0] / cutoff_radius);
+    int cellsY = std::ceil(domain_size[1] / cutoff_radius);
+    int cellsZ = std::ceil(domain_size[2] / cutoff_radius);
+
+    cells = std::vector<LinkedCell>();
+    inner = std::vector<LinkedCell>();
+    boundary = std::vector<LinkedCell>();
+    halo = std::vector<LinkedCell>();
+
+    for(int i = -1; i <= cellsX; i++) {
+        for(int j = -1; j <= cellsY; j++) {
+            for(int k = -1; k <= cellsZ; k++) {
+                std::array<double, 3> cellPos = {domain_pos[0] + i * cutoff_radius, domain_pos[1] + j * cutoff_radius, domain_pos[2] + k * cutoff_radius};
+
+                // constructor of LinkedCell checks if particles belong to cell -> TODO: more efficient way?
+                LinkedCell cell = LinkedCell(cellPos, cutoff_radius, particles);
+
+                if(i == -1 || i == cellsX || j == -1 || j == cellsY || k == -1 || k == cellsZ) {
+                    // cell is halo cell
+                    halo.emplace_back(cell);
+                } else if(i == 0 || i == cellsX - 1 || j == 0 || j == cellsY - 1 || k == 0 || k == cellsZ - 1) {
+                    // cell is boundary cell
+                    boundary.emplace_back(cell);
+                } else {
+                    //cell is inner cell
+                    inner.emplace_back(cell);
+                }
+
+                cells.emplace_back(cell);
+            }
+        }
+    }
 }
 
-void iterateNeighborCells(void (*f)(Particle&,Particle&)) {
+
+const std::array<double, 3> &LinkedCellContainer::getDomainSize() const {
+    return domain_size;
+}
+
+const std::array<double, 3> &LinkedCellContainer::getDomainPos() const {
+    return domain_pos;
+}
+
+double LinkedCellContainer::getCutoffRadius() const {
+    return cutoff_radius;
+}
+
+const std::vector<LinkedCell> &LinkedCellContainer::getCells() const {
+    return cells;
+}
+
+const std::vector<LinkedCell> &LinkedCellContainer::getInner() const {
+    return inner;
+}
+
+const std::vector<LinkedCell> &LinkedCellContainer::getBoundary() const {
+    return boundary;
+}
+
+const std::vector<LinkedCell> &LinkedCellContainer::getHalo() const {
+    return halo;
+}
+
+
+void LinkedCellContainer::iterateNeighborCells(void (*f)(LinkedCell&,LinkedCell&)) {
     for(auto i = cells.begin(); i != cells.end(); ++i)
-        for(auto j = i + 1; j != particles.end(); ++j)
-            (*f)(*i,*j);
+        for(auto j = i + 1; j != cells.end(); ++j)
+            if(i->isNeighbor(*j)) (*f)(*i,*j);
+}
+
+void LinkedCellContainer::deleteHaloParticles() {
+   for (int i = 0; i < cells.size(); i++) {
+        if (std::find(halo.begin(), halo.end(), cells.at(i)) != halo.end()) {
+
+            cells.at(i).setParticles(ParticleContainer());
+        }
+    }
 }
--- src/LinkedCellContainer.h
+++ src/LinkedCellContainer.h
@@ -9,19 +9,111 @@
 
 class LinkedCellContainer {
 private:
+    /**
+     * The size of the domain of the cells (inner + boundary)
+     */
+    std::array<double, 3> domain_size;
+
+    /**
+     * The position of the domain
+     */
+    std::array<double, 3> domain_pos; // TODO: do we need this?
+
+    /**
+     * THe cutoff-radius
+     */
     double cutoff_radius;
 
+    /**
+     * A vector containing all cells
+     */
     std::vector<LinkedCell> cells;
 
+    /**
+     * A vector containing only the inner cells
+     */
+    std::vector<LinkedCell> inner;
+
+    /**
+     * A vector containing only the boundary cells
+     */
     std::vector<LinkedCell> boundary;
 
+    /**
+     * A vector containing only the halo cells
+     */
     std::vector<LinkedCell> halo;
+
 public:
+
+    /**
+     * Default constructor
+     */
     LinkedCellContainer();
 
-    LinkedCellContainer(double cutoff_radius, ParticleContainer &particles);
+    /**
+     * Constructor for a LinkedCellContainer, creates cells automatically + assigns correct particles
+     * @param domain_size the domain size
+     * @param domain_pos the position of the domain
+     * @param cutoff_radius the cutoff radius
+     * @param particles the particles
+     */
+    LinkedCellContainer(std::array<double, 3> domain_size, std::array<double, 3> domain_pos, double cutoff_radius, ParticleContainer &particles);
 
-    void iterateParticlePairs(void (*f)(Particle&,Particle&));
+    /**
+     * Getter for the domain size
+     * @return the domain size
+     */
+    const std::array<double, 3> &getDomainSize() const;
+
+    /**
+     * Getter for the domain position
+     * @return the domain position
+     */
+    const std::array<double, 3> &getDomainPos() const;
+
+    /**
+     * Getter for the cutoff radius
+     * @return the cutoff radius
+     */
+    double getCutoffRadius() const;
+
+    /**
+     * Getter for the vector of cells
+     * @return the vector of cells
+     */
+    const std::vector<LinkedCell> &getCells() const;
+
+    /**
+     * Getter for the vector of inner cells
+     * @return the vector of inner cells
+     */
+    const std::vector<LinkedCell> &getInner() const;
+
+    /**
+     * Getter for the vector of boundary cells
+     * @return the vector of boundary cells
+     */
+    const std::vector<LinkedCell> &getBoundary() const;
+
+    /**
+     * Getter for the vector of halo cells
+     * @return the vector of halo cells
+     */
+    const std::vector<LinkedCell> &getHalo() const;
+
+
+
+    /**
+     *
+     * @param f
+     */
+    void iterateNeighborCells(void (*f)(LinkedCell&,LinkedCell&));
+
+    /**
+     * Deletes all the particles located in halo cells
+     */
+    void deleteHaloParticles();
 
 };
 
