dd8c6eb7f47e2a894d9d31a3b150d681e01fb159
Mira Trouvain <mira.trouvain@tum.de>
2020-12-09 13:10:53 +0100

    basic LinkedCell class

---
+++ src/LinkedCell.cpp
@@ -0,0 +1,50 @@
+//
+// Created by mira on 08.12.20.
+//
+
+#include "LinkedCell.h"
+
+
+LinkedCell::LinkedCell() {
+    position = {0,0,0};
+    cutoff = 1;
+    particles = ParticleContainer();
+}
+
+LinkedCell::LinkedCell(std::array<double, 3> position, double cutoff, ParticleContainer &particles) {
+    this->position = position;
+    this->cutoff = cutoff;
+    addParticles(particles);
+}
+
+
+std::array<double, 3> LinkedCell::getPosition() {
+    return position;
+}
+
+double LinkedCell::getCutoffRadius() {
+    return cutoff;
+}
+
+ParticleContainer LinkedCell::getParticles() {
+    return particles;
+}
+
+bool LinkedCell::particleBelongs(Particle &p) {
+    return (position[0] < p.getX()[0] && position[1] < p.getX()[1] && position[1] < p.getX()[1] &&
+            p.getX()[0] < position[0] + cutoff  && p.getX()[1] < position[1] + cutoff && p.getX()[2] < position[2] + cutoff);
+}
+
+bool LinkedCell::isNeighbor(LinkedCell &other) {
+    std::array<double, 3> diff = other.getPosition();
+    diff[0] -= position[0];
+    diff[1] -= position[1];
+    diff[2] -= position[2];
+    return (std::abs(diff[0]) == cutoff || std::abs(diff[1]) == cutoff || std::abs(diff[2]) == cutoff);
+}
+
+void LinkedCell::addParticles(ParticleContainer particles) {
+    for (auto &p : particles.getParticles()) {  // TODO: change to use iterate function?
+        if(particleBelongs(p)) particles.push(p);
+    }
+}
---
+++ src/LinkedCell.h
@@ -0,0 +1,87 @@
+//
+// Created by mira on 08.12.20.
+//
+
+#ifndef PSEMOLDYN_GROUPB_LINKEDCELL_H
+#define PSEMOLDYN_GROUPB_LINKEDCELL_H
+
+#include "ParticleContainer.h"
+
+class LinkedCell {
+private:
+    /**
+     * Coordinates of the lower front-left corner of ths cell.
+     */
+    std::array<double, 3> position;
+
+    /**
+     * the cutoff radius
+     */
+    double cutoff;
+
+    /**
+     * The particles located in this cell.
+     */
+    ParticleContainer particles;
+
+public:
+
+    /**
+     * Default constructor
+     */
+    LinkedCell();
+
+    /**
+     * Constructor for a cell
+     * @param position coordinates of lower front-left corner
+     * @param cutoff the cutoff radius
+     * @param particles particles to add to the cell
+     *
+     * The constructor adds the particles by calling addParticles(..),
+     * i.e. it is checked that the particles are actually locaed within the cell.
+     */
+    LinkedCell(std::array<double, 3> position, double cutoff, ParticleContainer &particles);
+
+    /**
+     * Getter for the position field
+     * @return the position
+     */
+    std::array<double, 3> getPosition();
+
+    /**
+     * Getter for the cutoff radius field
+     * @return the cutoff radius
+     */
+    double getCutoffRadius();
+
+    /**
+     * Getter for the particles field
+     * @return a ParticleContainer with the particles located in this cell
+     */
+    ParticleContainer getParticles();
+
+    /**
+     * Checks if another cell is a direct neighbor of the current cell.
+     * @param other the other cell
+     * @return true if the cells are neighbors, false otherwise
+     */
+    bool isNeighbor(LinkedCell &other);
+
+    /**
+     * Checks if the given particle is located within the bounds of this cell.
+     * @param p the particle in question
+     * @return true if the particle is located within this cell, false otherwise
+     */
+    bool particleBelongs(Particle &p);
+
+    /**
+     * Adds particles from given ParticleContainer to the particles stored in this cell
+     * if they belong.
+     * @param particles the ParticleContainer with particles to be added
+     */
+    void addParticles(ParticleContainer particles);
+
+};
+
+
+#endif //PSEMOLDYN_GROUPB_LINKEDCELL_H
