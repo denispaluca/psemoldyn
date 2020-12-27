2448d561d67b5e2c6e2dc95cb8001fb17782d9ad
denis <ge37dem@mytum.de>
2020-12-27 15:57:36 +0100

    Add gradual scale test
    Former-commit-id: f48975206261d60d447ee99e0de85ab9fbb71706

--- tests/src/ThermostatFixture.cpp
+++ tests/src/ThermostatFixture.cpp
@@ -13,6 +13,8 @@ protected:
     LinkedCellContainer lcc1;
     Thermostat* t2;
     LinkedCellContainer lcc2;
+    Thermostat* t3;
+    LinkedCellContainer lcc3;
     void SetUp() override{
         std::unique_ptr<molsimInput> data;
         ParticleContainer pc;
@@ -26,10 +28,16 @@ protected:
         t2 = new Thermostat(data->thermostat().get(), data->particle_data().is3D());
         pc = ParticleGenerator(data->particle_data(), t2).getParticles();
         lcc2 = LinkedCellContainer(data->domain(), pc);
+
+        data = input("input/input_thermostat_fixture_delta.xml");
+        t3 = new Thermostat(data->thermostat().get(), data->particle_data().is3D());
+        pc = ParticleGenerator(data->particle_data(), t3).getParticles();
+        lcc3 = LinkedCellContainer(data->domain(), pc);
     }
 };
 
 TEST_F(ThermostatFixture, Scales){
+    EXPECT_NEAR(t1->getCurrentTemp(lcc1), 0, 0.1); //Change from brownian motion
     for(int i = 0; i < 100; i++){
         lcc1.calculateIteration();
     }
@@ -42,5 +50,22 @@ TEST_F(ThermostatFixture, Scales){
 
 TEST_F(ThermostatFixture, InitTemp_SET){
     auto t_init = t2->getCurrentTemp(lcc2);
-    EXPECT_DOUBLE_EQ(t_init, t2->getInitTemp());
+    EXPECT_NEAR(t_init, t2->getInitTemp(), 5); //Brownian motion rand
+}
+
+
+TEST_F(ThermostatFixture, Gradual_Scale){
+    EXPECT_NEAR(t3->getCurrentTemp(lcc3), 0, 0.1); //Change from brownian motion
+    for(int i = 0; i < 100; i++){
+        lcc3.calculateIteration();
+    }
+    auto temp1 = t3->getCurrentTemp(lcc3);
+    t3->scale(lcc3);
+    auto t_init = t3->getCurrentTemp(lcc3);
+    EXPECT_NE(temp1, t_init);
+    EXPECT_NE(t_init, 100);
+    EXPECT_NEAR(t_init, 10, 0.1);
+    t3->scale(lcc3);
+    t_init = t3->getCurrentTemp(lcc3);
+    EXPECT_NEAR(t_init, 20, 0.1);
 }
