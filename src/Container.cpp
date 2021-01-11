3db92c65dafbd65bd73eaea7cf4b7be09511ae5a
Mira Trouvain <mira.trouvain@tum.de>
2021-01-11 20:16:18 +0100

    added getters in Container for tests
    Former-commit-id: 4052e6b4117da7db0a1b9401a0161310d93dea5a

---
+++ src/Container.cpp
@@ -0,0 +1,13 @@
+//
+// Created by mira on 11.01.21.
+//
+
+#include "Container.h"
+
+std::map<std::array<double, 2>, double> Container::getMixedEpsilon() {
+    return mixedEpsilon;
+}
+
+std::map<std::array<double, 2>, double> Container::getMixedSigma() {
+    return mixedSigma;
+}
